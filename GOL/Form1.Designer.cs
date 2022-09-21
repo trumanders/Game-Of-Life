e80cb441cf69e2cf0ea2437419ee42b90c45bbdb
trumanders <trumanders@gmail.com>
2022-09-21 12:13:47 +0200

    First commit - original version

---
+++ GOL/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace GOL
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtCellCount = new System.Windows.Forms.TextBox();
+            this.txtAvgFrameTime = new System.Windows.Forms.TextBox();
+            this.txtNumOfStartCells = new System.Windows.Forms.TextBox();
+            this.txtGens = new System.Windows.Forms.TextBox();
+            this.txtGrowthPercent = new System.Windows.Forms.TextBox();
+            this.txtGrowthCells = new System.Windows.Forms.TextBox();
+            this.txtMaxGrowthPercent = new System.Windows.Forms.TextBox();
+            this.txtMaxGrowthCells = new System.Windows.Forms.TextBox();
+            this.txtMaxGrowthGeneration = new System.Windows.Forms.TextBox();
+            this.txtMaxCellCount = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtCellCount
+            // 
+            this.txtCellCount.BackColor = System.Drawing.Color.Black;
+            this.txtCellCount.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.txtCellCount.ForeColor = System.Drawing.Color.White;
+            this.txtCellCount.Location = new System.Drawing.Point(168, 12);
+            this.txtCellCount.Name = "txtCellCount";
+            this.txtCellCount.Size = new System.Drawing.Size(142, 25);
+            this.txtCellCount.TabIndex = 1;
+            // 
+            // txtAvgFrameTime
+            // 
+            this.txtAvgFrameTime.BackColor = System.Drawing.Color.Black;
+            this.txtAvgFrameTime.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.txtAvgFrameTime.ForeColor = System.Drawing.Color.White;
+            this.txtAvgFrameTime.Location = new System.Drawing.Point(23, 41);
+            this.txtAvgFrameTime.Name = "txtAvgFrameTime";
+            this.txtAvgFrameTime.Size = new System.Drawing.Size(139, 25);
+            this.txtAvgFrameTime.TabIndex = 2;
+            this.txtAvgFrameTime.TextChanged += new System.EventHandler(this.txtAvgFrameTime_TextChanged);
+            // 
+            // txtNumOfStartCells
+            // 
+            this.txtNumOfStartCells.BackColor = System.Drawing.Color.Black;
+            this.txtNumOfStartCells.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.txtNumOfStartCells.ForeColor = System.Drawing.Color.White;
+            this.txtNumOfStartCells.Location = new System.Drawing.Point(23, 12);
+            this.txtNumOfStartCells.Name = "txtNumOfStartCells";
+            this.txtNumOfStartCells.Size = new System.Drawing.Size(139, 25);
+            this.txtNumOfStartCells.TabIndex = 3;
+            // 
+            // txtGens
+            // 
+            this.txtGens.BackColor = System.Drawing.Color.Black;
+            this.txtGens.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.txtGens.ForeColor = System.Drawing.Color.White;
+            this.txtGens.Location = new System.Drawing.Point(168, 41);
+            this.txtGens.Name = "txtGens";
+            this.txtGens.Size = new System.Drawing.Size(142, 25);
+            this.txtGens.TabIndex = 4;
+            // 
+            // txtGrowthPercent
+            // 
+            this.txtGrowthPercent.BackColor = System.Drawing.Color.Black;
+            this.txtGrowthPercent.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.txtGrowthPercent.ForeColor = System.Drawing.Color.White;
+            this.txtGrowthPercent.Location = new System.Drawing.Point(316, 12);
+            this.txtGrowthPercent.Name = "txtGrowthPercent";
+            this.txtGrowthPercent.Size = new System.Drawing.Size(132, 25);
+            this.txtGrowthPercent.TabIndex = 5;
+            this.txtGrowthPercent.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // txtGrowthCells
+            // 
+            this.txtGrowthCells.BackColor = System.Drawing.Color.Black;
+            this.txtGrowthCells.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.txtGrowthCells.ForeColor = System.Drawing.Color.White;
+            this.txtGrowthCells.Location = new System.Drawing.Point(454, 12);
+            this.txtGrowthCells.Name = "txtGrowthCells";
+            this.txtGrowthCells.Size = new System.Drawing.Size(161, 25);
+            this.txtGrowthCells.TabIndex = 6;
+            // 
+            // txtMaxGrowthPercent
+            // 
+            this.txtMaxGrowthPercent.BackColor = System.Drawing.Color.Black;
+            this.txtMaxGrowthPercent.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.txtMaxGrowthPercent.ForeColor = System.Drawing.Color.White;
+            this.txtMaxGrowthPercent.Location = new System.Drawing.Point(316, 41);
+            this.txtMaxGrowthPercent.Name = "txtMaxGrowthPercent";
+            this.txtMaxGrowthPercent.Size = new System.Drawing.Size(132, 25);
+            this.txtMaxGrowthPercent.TabIndex = 7;
+            // 
+            // txtMaxGrowthCells
+            // 
+            this.txtMaxGrowthCells.BackColor = System.Drawing.Color.Black;
+            this.txtMaxGrowthCells.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.txtMaxGrowthCells.ForeColor = System.Drawing.Color.White;
+            this.txtMaxGrowthCells.Location = new System.Drawing.Point(454, 41);
+            this.txtMaxGrowthCells.Name = "txtMaxGrowthCells";
+            this.txtMaxGrowthCells.Size = new System.Drawing.Size(161, 25);
+            this.txtMaxGrowthCells.TabIndex = 8;
+            // 
+            // txtMaxGrowthGeneration
+            // 
+            this.txtMaxGrowthGeneration.BackColor = System.Drawing.Color.Black;
+            this.txtMaxGrowthGeneration.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.txtMaxGrowthGeneration.ForeColor = System.Drawing.Color.White;
+            this.txtMaxGrowthGeneration.Location = new System.Drawing.Point(621, 41);
+            this.txtMaxGrowthGeneration.Name = "txtMaxGrowthGeneration";
+            this.txtMaxGrowthGeneration.Size = new System.Drawing.Size(167, 25);
+            this.txtMaxGrowthGeneration.TabIndex = 9;
+            // 
+            // txtMaxCellCount
+            // 
+            this.txtMaxCellCount.BackColor = System.Drawing.Color.Black;
+            this.txtMaxCellCount.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.txtMaxCellCount.ForeColor = System.Drawing.Color.White;
+            this.txtMaxCellCount.Location = new System.Drawing.Point(621, 12);
+            this.txtMaxCellCount.Name = "txtMaxCellCount";
+            this.txtMaxCellCount.Size = new System.Drawing.Size(167, 25);
+            this.txtMaxCellCount.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1656, 891);
+            this.Controls.Add(this.txtMaxCellCount);
+            this.Controls.Add(this.txtMaxGrowthGeneration);
+            this.Controls.Add(this.txtMaxGrowthCells);
+            this.Controls.Add(this.txtMaxGrowthPercent);
+            this.Controls.Add(this.txtGrowthCells);
+            this.Controls.Add(this.txtGrowthPercent);
+            this.Controls.Add(this.txtGens);
+            this.Controls.Add(this.txtNumOfStartCells);
+            this.Controls.Add(this.txtAvgFrameTime);
+            this.Controls.Add(this.txtCellCount);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private TextBox txtCellCount;
+        private TextBox txtAvgFrameTime;
+        private TextBox txtNumOfStartCells;
+        private TextBox txtGens;
+        private TextBox txtGrowthPercent;
+        private TextBox txtGrowthCells;
+        private TextBox txtMaxGrowthPercent;
+        private TextBox txtMaxGrowthCells;
+        private TextBox txtMaxGrowthGeneration;
+        private TextBox txtMaxCellCount;
+    }
+}
---
+++ GOL/Form1.cs
@@ -0,0 +1,449 @@
+
+using System.Diagnostics;
+using Microsoft.VisualBasic;
+    namespace GOL
+{
+    // Rita ut levande celler -> de läggs i en 1D-list med Points aliveCellsPoints.
+    // Bara de levande cellerna i aliveCellsPoints beräknas på. Har de 2 eller 3 grannar så överlever de.
+    // Varje levande cells döda grannar kollas. Har de 3 grannar så kommer de till liv. Läggs till i aliveCellsPoints.
+    // I aliveCellsPoints kan man inte kolla efter grannar på ett enkelt sätt. Därför läggs alla levande till i en 2D array: 
+    // aliveDeadChecker. 1=alive 0=dead. Denna itereras inte genom i sin helhet, utan används för att kolla grannar.
+    // Eftersom samma döda granne kan evalueras flera gånger  måste det kollas om den cellen redan lags till i makeAlive.
+    // (isDuplicate).
+
+
+    public partial class Form1 : Form
+    {
+        const string CELLCOUNT = "Cell Count: ";
+        const string AVG_FRAME_TIME = "Avg Frame Time: ";
+        const string INIT_CELLS = "Init. Cells: ";
+        const string NUM_OF_GENERATIONS = "Num of gens: ";
+        const int FORM_RESOLUTION_X = 2000;
+        const int FORM_RESOLUTION_Y = 2000;
+        const int SECOND = 1000;
+        int genPerSec = 20;
+        bool pencilDown;
+        Point gridResolution;      
+        int gridSquareSize;
+        Rectangle[,] rectGrid;
+        List<Point> aliveCellsPoints;       
+        int[,] aliveDeadChecker;
+        Brush deadBrush;
+        Brush aliveBrush;
+        Graphics g;
+        bool eraseMode;     // Click dead cell: eraseMode = false. Click living cell: eraseMode = true
+        double cellCount;
+        int generationCount;
+        double frameCount;
+        double initialCells;
+        double frameTimeSum;
+        double maxPercent = -2000000000;
+        double maxCells = -2000000000;
+        int maxGrowthGeneration;
+        double maxCellCount = 0;
+
+
+        // Constructor
+        public Form1()
+        {
+            DoubleBuffered = true;
+            InitializeComponent();
+            aliveCellsPoints = new List<Point>();
+            pencilDown = false;
+            this.Width = FORM_RESOLUTION_X;
+            this.Height = FORM_RESOLUTION_Y;
+            gridResolution = new Point(400,400);
+            gridSquareSize = this.Width / gridResolution.X;
+            rectGrid = new Rectangle[gridResolution.X, gridResolution.Y];            
+            aliveDeadChecker = new int[gridResolution.X, gridResolution.Y]; 
+            deadBrush = new SolidBrush(Color.Black);
+            aliveBrush = new SolidBrush(Color.White);
+            cellCount = 0;
+            generationCount = 1;
+
+            // Initialize the grid rectangles
+            for (int i = 0; i < rectGrid.GetLength(0); i++)
+            {
+                for (int j = 0; j < rectGrid.GetLength(1); j++)
+                {
+                    rectGrid[i, j] = new Rectangle(i * gridSquareSize, j * gridSquareSize, gridSquareSize, gridSquareSize);
+                }
+            }
+            string input = Interaction.InputBox("Prompt", "Title", "Default", 10, 10);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            g = e.Graphics;
+            g.Clear(Color.Black);
+        }
+
+
+
+        // Update cell at mouse click
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            Graphics g = this.CreateGraphics();
+            pencilDown = true;
+
+            // Grid coordinates
+            int x = e.X / gridSquareSize;
+            int y = e.Y / gridSquareSize;
+
+            // Handle draw outside grid
+            x = DrawOutsideGrid(x);
+            y = DrawOutsideGrid(y);
+
+            // Add cell if clicked dead cell
+            if (aliveDeadChecker[x, y] == 0)
+                AddCell(x, y, g);
+
+            // Remove cell if clicked living cell, and activate erase mode
+            else
+            {                
+                RemoveCell(x, y, g);
+                eraseMode = true;                
+            }
+            UpdateText();
+        }
+
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            pencilDown = false;
+            eraseMode = false;
+        }
+
+                
+        // Update cell on MOUSE MOVE
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            Graphics g = this.CreateGraphics();
+
+            // Grid coordinates
+            int x = e.X / gridSquareSize;
+            int y = e.Y / gridSquareSize;
+
+            // If mouse button down
+            if (pencilDown)
+            {
+                x = DrawOutsideGrid(x);
+                y = DrawOutsideGrid(y);
+
+
+                // If not erasing cells and moving to dead cell, then add cell
+                if (!eraseMode && aliveDeadChecker[x, y] == 0)
+                    AddCell(x, y, g);
+             
+                // If erasing cells, and moving to living cell, then remove cell
+                else if (eraseMode && aliveDeadChecker[x, y] == 1)
+                    RemoveCell(x, y, g);
+                UpdateText();
+            }
+        }
+
+
+        // Update the statistics
+        private void UpdateText()
+        {
+            txtCellCount.Text = CELLCOUNT + cellCount.ToString();
+            txtNumOfStartCells.Text = "Initial cells: " + cellCount.ToString();
+        }
+
+
+        // Method to handle drawing outside of the grid
+        private int DrawOutsideGrid(int value)
+        {            
+            if (value > gridResolution.X - 1) value = gridResolution.X - 1;
+            if (value < 0) value = 0;
+            return value;
+        }
+
+
+        // Method to add and draw cell
+        private void AddCell(int x, int y, Graphics g)
+        {
+            aliveCellsPoints.Add(new Point(x, y));
+            aliveDeadChecker[x, y] = 1;
+            g.FillRectangle(aliveBrush, rectGrid[x, y]);
+            cellCount++;
+        }
+
+
+        // Method to remove and draw dead cell
+        private void RemoveCell(int x, int y, Graphics g)
+        {
+            aliveDeadChecker[x, y] = 0;
+            for (int i = 0; i < aliveCellsPoints.Count; i++)
+            {
+                if (aliveCellsPoints[i].X == x && aliveCellsPoints[i].Y == y)
+                    aliveCellsPoints.RemoveAt(i);
+            }
+            g.FillRectangle(deadBrush, rectGrid[x, y]);
+            cellCount--;
+        }
+
+
+        // Listen for key stroke
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == ' ')
+            {
+                runSimLoop();
+            }
+
+            if (e.KeyChar == 'q')
+            {
+                this.Close();
+            }
+        }
+
+
+        // START SIMULATION
+        private void runSimLoop()
+        {
+            List<Point> kill = new List<Point>();       // Store coordinates of cells to remove
+            List<Point> makeAlive = new List<Point>();  // Store coordinates of cells to make alive
+            List<int> killIndexes = new List<int>();    // Store indexes of alive cells to remove
+
+            // Set statistics
+            initialCells = cellCount;
+            maxCellCount = cellCount;
+            //Graphics g = e.Graphics;
+            Graphics g = this.CreateGraphics();
+
+
+            // START LOOP
+            
+            txtNumOfStartCells.Text = initialCells.ToString();
+            //txtNumOfStartCells.Refresh();
+            while (true)
+            {
+                // Set current time to measure loop duration
+                DateTime startTime = DateTime.Now;           
+                
+                // Iterate through living cells only
+                for (int i = 0; i < aliveCellsPoints.Count; i++) 
+                {                    
+                    int neighbourCount = countNeighbours(aliveCellsPoints[i], gridResolution, aliveDeadChecker);
+
+                    // If living cell has < 2 or > 3 neighbours, add cell to be removed
+                    if (neighbourCount < 2 || neighbourCount > 3)
+                    {
+                        // Check if this alive cell is already added to be removed...
+                        if (!isDuplicate(kill, aliveCellsPoints[i]))
+                        {
+                            // ...if not, add cell to kill list
+                            kill.Add(aliveCellsPoints[i]);
+                        }
+
+                        // Store index to remove later in aliveCellsPoints
+                        killIndexes.Add(i);
+                    }
+
+                    // Now iterate through cells around current alive cell (i)...
+                    for (int ii = aliveCellsPoints[i].X - 1; ii <= aliveCellsPoints[i].X + 1; ii++)
+                    {
+                        // Don't check outside of the grid
+                        if (ii < 0 || ii > gridResolution.X - 1) { continue; }
+                        
+                        for (int jj = aliveCellsPoints[i].Y - 1; jj <= aliveCellsPoints[i].Y + 1; jj++)
+                        {
+                            if (jj < 0 || jj > gridResolution.X - 1) { continue; }
+
+                            // Don't include the cell itself
+                            if (ii == aliveCellsPoints[i].X && jj == aliveCellsPoints[i].Y) { continue; }
+                            
+                            // If the neighbour is dead AND has 3 neighbours, make it alive
+                            if (aliveDeadChecker[ii,jj] == 0 && countNeighbours(new Point(ii, jj), gridResolution, aliveDeadChecker) == 3)
+                            {
+                                // Add the dead neighbour to makeAlive if it's not already added
+                                if (!isDuplicate(makeAlive, new Point(ii, jj)))
+                                {
+                                    makeAlive.Add(new Point(ii, jj));
+                                }
+                            }                           
+                        }
+                    }
+                }
+
+                // Make changes: dead -> alive 
+                for (int i = 0; i < makeAlive.Count; i++)
+                {                   
+                    aliveCellsPoints.Add(new Point(makeAlive[i].X, makeAlive[i].Y));
+                    g.FillRectangle(aliveBrush, rectGrid[makeAlive[i].X, makeAlive[i].Y]);
+
+                    // Update aliveDeadChecker
+                    aliveDeadChecker[makeAlive[i].X, makeAlive[i].Y] = 1;
+                    cellCount++;
+                }
+
+                // Make changes: alive -> dead
+                if (kill.Count > 0) 
+                {
+                    for (int i = 0; i < kill.Count; i++)
+                    {
+                        g.FillRectangle(deadBrush, rectGrid[kill[i].X, kill[i].Y]);
+
+                        // update aliveDeadChecker
+                        aliveDeadChecker[kill[i].X, kill[i].Y] = 0;
+                        cellCount--;
+                    }
+
+                    // Remove from aliveCellsPoints
+                    for (int i = killIndexes.Count - 1; i >= 0; i--)
+                    {
+                        aliveCellsPoints.RemoveAt(killIndexes[i]);
+                    }
+                }                
+                
+                // clear stuff
+                //Thread.Sleep(SECOND / genPerSec);               
+                kill.Clear();
+                makeAlive.Clear();
+                killIndexes.Clear();
+
+                // Measure loop time
+                DateTime endTime = DateTime.Now;
+                TimeSpan frameTime = endTime - startTime;
+                frameCount++;
+                frameTimeSum += frameTime.TotalMilliseconds;
+                generationCount++;
+                displayText();              
+            }
+        }
+
+        double calcMaxCellCount()
+        {
+            if (cellCount > maxCellCount)
+            {
+                maxCellCount = cellCount;
+            }
+            return maxCellCount;
+        }
+
+        double growthPercent()
+        {
+            return ((cellCount - initialCells) / initialCells) * 100;
+        }
+
+        double growthCells()
+        {
+            return cellCount - initialCells;
+        }
+
+        double maxGrowthPercent()
+        {
+            double growth = growthPercent();
+            if (growth > maxPercent)
+            {
+                maxPercent = growth;
+                maxGrowthGeneration = generationCount;
+            }
+            return maxPercent;
+        }
+
+        double maxGrowthCells()
+        {
+            double growth = growthCells();
+            if (growth > maxCells)
+            {
+                maxCells = growth;
+            }
+            return maxCells;
+        }
+
+
+        void displayText()
+        {
+            txtCellCount.Text = "CELL COUNT: " + cellCount.ToString();
+            txtAvgFrameTime.Text = "AVG Frame Dur.: " + (Math.Round(frameTimeSum / frameCount)).ToString();
+            txtGens.Text = "Gen. Count: " + generationCount;
+            txtNumOfStartCells.Text = "Initial cells: " + initialCells.ToString();
+            txtGrowthPercent.Text = "Growth %: " + Math.Round(growthPercent()).ToString();
+            txtGrowthCells.Text = "Growth cells: " + Math.Round(growthCells()).ToString();
+            txtMaxGrowthPercent.Text = "Max growth %: " + Math.Round(maxGrowthPercent()).ToString();
+            txtMaxGrowthCells.Text = "Max growth cells: " + Math.Round(maxGrowthCells()).ToString();
+            txtMaxGrowthGeneration.Text = "Max growth at gen: " + maxGrowthGeneration;
+            txtMaxGrowthGeneration.Refresh();
+            txtMaxCellCount.Text = "Max cell count: " + calcMaxCellCount().ToString();
+            txtMaxCellCount.Refresh();
+            txtGens.Refresh();
+            txtCellCount.Refresh();
+            txtAvgFrameTime.Refresh();
+            txtGrowthPercent.Refresh();
+            txtGrowthCells.Refresh();
+            txtMaxGrowthCells.Refresh();
+            txtMaxGrowthPercent.Refresh();
+        }
+
+        bool isDuplicate(List<Point> checkForDuplicate, Point checkPoint)
+        {
+            for (int i = 0; i < checkForDuplicate.Count; i++)
+            {
+                // if same point already added
+                if (checkForDuplicate[i].X == checkPoint.X && checkForDuplicate[i].Y == checkPoint.Y)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+
+        void updateAliveDeadChecker(int[,] toBeUpdated, int[,] update)
+        {
+            for (int a = 0; a < toBeUpdated.GetLength(0); a++)
+            {
+                for (int b = 0; b < toBeUpdated.GetLength(1); b++)
+                {
+                    toBeUpdated[a, b] = update[a, b];
+                }
+            }
+        }
+
+
+        int countNeighbours(Point checkPoint, Point gridResolution, int[,] aliveDeadChecker)
+        {
+            int neighboursCount = 0;
+
+            // Check one to the left  - to - one to the right
+            for (int i = checkPoint.X - 1; i <= checkPoint.X + 1; i++)
+            {
+                
+                if (i < 0 || i > gridResolution.X - 1) { continue; }
+
+                // Check one the the left - to - one to the right
+                for (int j = checkPoint.Y - 1; j <= checkPoint.Y + 1; j++)
+                {
+                    if (j < 0 || j > gridResolution.Y - 1) { continue; }
+                    if (i == checkPoint.X && j == checkPoint.Y) { continue; }
+                    if (aliveDeadChecker[i, j] == 1) { neighboursCount++; }
+                }
+            }
+            return neighboursCount;
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtAvgFrameTime_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
